e6b2a888109e8f4096421fba1b4fb3825720cde2
virtualabishek <717abishekneupane@gmail.com>
2025-07-23 10:07:25 +0545

    add: dotnet basic

--- dotnet-programming/projects/MyMvcApp/Controllers/StudentController.cs
+++ dotnet-programming/projects/MyMvcApp/Controllers/StudentController.cs
@@ -5,16 +5,8 @@ namespace MyMvcApp.Controllers
 {
     public class StudentController : Controller
     {
-        public IActionResult Index()
-        {
-            var students = new List<Student>
-            {
-                new Student { Id = 1, Name = "Abi Neupane", Course = "BIT" },
-                new Student { Id = 2, Name = "Rohit Sharma", Course = "CSIT" },
-                new Student {Id = 3, Name = "Haru Thapa", Course = "BIM"}
-            };
-
-            return View(students);
-        }
+       public String Index() {
+        return "This is test mvc";
+       }
     }
 }
--- dotnet-programming/projects/MyMvcApp/Models/Student.cs
+++ dotnet-programming/projects/MyMvcApp/Models/Student.cs
@@ -2,8 +2,16 @@ namespace MyMvcApp.Models
 {
     public class Student
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Course { get; set; }
+        public int id {get: set};
+        public string name {get: set};
+        public string address {get: set};
+    }
+    public class dataAccess {
+        public Student getById(int id) {
+            Student sm = new Student();
+            sm.id = id;
+            sm.name = name;
+            sm.address = address;
+        }
     }
 }
