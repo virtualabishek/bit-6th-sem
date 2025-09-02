81fbda57ba24f4cf169a717f7a3e9541306fd31b
virtualabishek <717abishekneupane@gmail.com>
2025-09-02 07:24:13 +0545

    add: exploring OOPS concepts

--- dotnet-programming/C# Basic/testme/Program.cs
+++ dotnet-programming/C# Basic/testme/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
 Console.WriteLine(3 + 4);
 
 string fname = "Hari";
@@ -15,7 +16,7 @@ Console.WriteLine(pi);
 string firstName = "Abi";
 string lastName = "Fix You";
 
-Console.WriteLine("Name is: " + firstName + " " +  lastName);
+Console.WriteLine("Name is: " + firstName + " " + lastName);
 
 int a = 10, b = 20;
 
@@ -47,3 +48,57 @@ else
 {
     Console.WriteLine($"{a} + {b} equals 0.3");
 }
+
+
+// Console.WriteLine("Enter your username: ");
+// string userName = Console.ReadLine();
+
+// Console.WriteLine("Your name is: " + userName);
+
+// // Console always take input as the string so need to convert using the type implicit
+
+// Console.WriteLine("Enter the number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("You just entered: " + num);
+
+string[] nameC = new String[5];
+
+nameC = ["Ram", "Hari"];
+Console.WriteLine(nameC[1]);
+
+string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+Array.Sort(cars);
+foreach (string i in cars)
+{
+    Console.WriteLine(i);
+}
+
+int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+foreach (int i in numbers)
+{
+    Console.WriteLine(i);
+}
+
+// Method Overloading
+
+// static int PlusMethod(int x, int y)
+// {
+//     return x + y;
+// }
+
+// static double PlusMethod(double x, double y)
+// {
+//     return x + y;
+// }
+
+
+// int val1 = PlusMethod(1, 2);
+
+// double val2 = PlusMethod(2.44, 2.4);
+
+// Console.WriteLine("Int: " + val1);
+// Console.WriteLine("Float: " + val2);
+
+// This above works only inside the class.
